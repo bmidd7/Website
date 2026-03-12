be062e8ce3474fca1d5149e0b16cd90a94967cfb
Bradley Middleton <bmmidd227@gmail.com>
2026-03-11 20:28:22 -0500

    got start to chem gases page working. added input spots

--- All_Websites/static/js/School/Chem/Gases.js
+++ All_Websites/static/js/School/Chem/Gases.js
@@ -1,2 +1,29 @@
-const beforeVolumeGiven = document.getElementById(id="before-volume-given")
-const beforePressureGiven = document.getElementById(id="before-pressure-given")
+//GB...  (Given (Before), Type, Unit/)
+const GBV = [
+  document.getElementById("before-volume-given").value,
+  document.getElementById("before-volume-unit-given").value,
+];
+const GBP = [
+  document.getElementById("before-pressure-given").value,
+  document.getElementById("before-pressure-unit-given").value,
+];
+const GBT = [
+  document.getElementById("before-temperature-given").value,
+  document.getElementById("before-temperature-unit-given").value,
+];
+
+//GA... (Given (After), Type, Unit/)
+const GAV = [
+  document.getElementById("after-volume-given").value,
+  document.getElementById("after-volume-unit-given").value,
+];
+const GAP = [
+  document.getElementById("after-pressure-given").value,
+  document.getElementById("after-pressure-unit-given").value,
+];
+const GAT = [
+  document.getElementById("after-temperature-given").value,
+  document.getElementById("after-temperature-unit-given").value,
+];
+
+for 
