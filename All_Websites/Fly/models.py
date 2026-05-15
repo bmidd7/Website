862fade92d06b4cb84b205871ec53f594f7faf8e
Bradley Middleton <bmmidd227@gmail.com>
2026-05-15 01:18:13 -0500

    Nothing

--- All_Websites/Fly/models.py
+++ All_Websites/Fly/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
 class RawData(models.Model):
@@ -9,7 +10,24 @@ class RawData(models.Model):
     quality = models.CharField(default="preview")
     world = models.CharField(default="arena")
     behavior=models.CharField(default="wander")
-    seed=models.IntegerField(default=1, max_value=4294967295) #32-bit, usigned int limit
+    seed=models.IntegerField(default=1, validators=[MaxValueValidator(4294967295)]) #32-bit, unsigned int limit
     arena_size=models.FloatField(default=10.0)
     data_path=models.CharField(default=r"m:/C VSCode/.vscode/Fly/proofread_connections_783.feather")
     max_saved_frames=models.IntegerField(default=500)
+    #resource usage
+    #CPU
+    # max
+    # avg
+    # min
+    #GPU
+    # max
+    # avg
+    # min
+    #VRAM
+    # max
+    # avg
+    # min
+    #RAM
+    # max
+    # avg
+    # min
